dc2325b9d72fa223f98d36629fb30318e20a3578
digitar120 <digitar120@outlook.com>
2023-06-12 14:30:29 -0300

    Commit inicial

---
+++ src/main/java/com/digitar120/ejercicio1rehecho/Ejercicio1RehechoApplication.java
@@ -0,0 +1,13 @@
+package com.digitar120.ejercicio1rehecho;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Ejercicio1RehechoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Ejercicio1RehechoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/digitar120/ejercicio1rehecho/Ejercicio1RehechoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.digitar120.ejercicio1rehecho;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Ejercicio1RehechoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
