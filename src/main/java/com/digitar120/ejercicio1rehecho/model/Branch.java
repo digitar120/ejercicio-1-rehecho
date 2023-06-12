40c42575d2a7a8110dde76347e60a027ef3d0c88
digitar120 <digitar120@outlook.com>
2023-06-12 16:45:24 -0300

    Añadido un modelo Person

---
+++ src/main/java/com/digitar120/ejercicio1rehecho/model/Branch.java
@@ -0,0 +1,4 @@
+package com.digitar120.ejercicio1rehecho.model;
+
+public class Branch {
+}
--- src/main/java/com/digitar120/ejercicio1rehecho/model/Person.java
+++ src/main/java/com/digitar120/ejercicio1rehecho/model/Person.java
@@ -1,13 +1,52 @@
 package com.digitar120.ejercicio1rehecho.model;
 
-import jakarta.persistence.Column;
-import jakarta.persistence.Entity;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.Id;
+import jakarta.persistence.*;
 
 @Entity
 public class Person {
     private @Id @GeneratedValue @Column(name = "PERSON_ID") Integer id;
     private @Column(name = "PERSON_NAME") String name;
-    private
+    private @ManyToOne @JoinColumn(name="BRANCH_ID") Branch branch;
+
+    public Person(Integer id, String name, Branch branch) {
+        this.id = id;
+        this.name = name;
+        this.branch = branch;
+    }
+
+    public Person() {
+    }
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public Branch getBranch() {
+        return branch;
+    }
+
+    public void setBranch(Branch branch) {
+        this.branch = branch;
+    }
+
+    @Override
+    public String toString() {
+        return "Person{" +
+                "id=" + id +
+                ", name='" + name + '\'' +
+                ", branch=" + branch +
+                '}';
+    }
 }
