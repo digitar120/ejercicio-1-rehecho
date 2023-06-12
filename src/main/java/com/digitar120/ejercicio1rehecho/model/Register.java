0df457d74e24ad0746dfd7e5c16350c100395a2a
digitar120 <digitar120@outlook.com>
2023-06-12 16:33:52 -0300

    Agregado un modelo Register

---
+++ src/main/java/com/digitar120/ejercicio1rehecho/model/Register.java
@@ -0,0 +1,55 @@
+package com.digitar120.ejercicio1rehecho.model;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.Id;
+
+@Entity
+public class Register {
+       private @Id @GeneratedValue @Column(name = "REGISTER_ID") Integer id;
+       private @Column (name = "REGISTER_TEXT") String text;
+       private @Column(name = "PERSON_ID") Person person;
+
+    public Register(Integer id, String text, Person person) {
+        this.id = id;
+        this.text = text;
+        this.person = person;
+    }
+
+    public Register() {
+    }
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getText() {
+        return text;
+    }
+
+    public void setText(String text) {
+        this.text = text;
+    }
+
+    public Person getPerson() {
+        return person;
+    }
+
+    public void setPerson(Person person) {
+        this.person = person;
+    }
+
+    @Override
+    public String toString() {
+        return "Register{" +
+                "id=" + id +
+                ", text='" + text + '\'' +
+                ", person=" + person +
+                '}';
+    }
+}
